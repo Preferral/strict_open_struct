d6e7cda2771b1680c2d7e949e5ce25776ca01f38
Nathan Wenneker <nathan@preferral.com>
2016-03-15 08:31:50 -0600

    Refactor. Also remove unnecessary respond_to?

--- lib/strict_open_struct.rb
+++ lib/strict_open_struct.rb
@@ -9,19 +9,15 @@ class StrictOpenStruct
   # Raises NoMethodError unless the underlying OpenStruct
   # responds to the method or the method is a setter
   def method_missing(method_name, *args, &block)
-    if respond_to_missing?(method_name)
+    if open_struct_responds_to?(method_name)
       @open_struct.send(method_name, *args, &block)
     else
       fail NoMethodError, "undefined method `#{method_name}' for #{self}"
     end
   end
 
-  def respond_to?(method_name, include_private = false)
-    super || respond_to_missing?(method_name, include_private)
-  end
-
   def respond_to_missing?(method_name, include_private = false)
-    @open_struct.respond_to?(method_name) || method_name =~ /=$/
+    open_struct_responds_to?(method_name)
   end
 
   def ==(other)
@@ -31,4 +27,11 @@ class StrictOpenStruct
   def [](key)
     self.send(key)
   end
+
+  private
+
+  def open_struct_responds_to?(method_name)
+    @open_struct.respond_to?(method_name) || method_name =~ /=$/
+  end
+
 end
